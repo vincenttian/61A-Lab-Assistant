f5f63853c25a0432ff7d82f5d77fab0a9973c427
Vincent Tian <vincenttian16@gmail.com>
2015-03-12 22:30:22 -0700

    action mailer fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,8 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
@@ -59,6 +61,9 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.default_url_options = { :host => 'cs61a-lab-assistant.herokuapp.com' }
+
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
