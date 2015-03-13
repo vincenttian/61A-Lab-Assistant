c941fa4ec6dceaf84cece4da7e0984a21a3e11b1
Vincent Tian <vincenttian16@gmail.com>
2015-03-13 14:15:29 -0700

    fix rspec/travis testing stuff

--- spec/controllers/check_in_forms_controller_spec.rb
+++ spec/controllers/check_in_forms_controller_spec.rb
@@ -9,41 +9,6 @@ RSpec.describe CheckInFormsController, :type => :controller do
     end
   end
 
-  describe "GET create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET edit" do
-    it "returns http success" do
-      get :edit
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET update" do
-    it "returns http success" do
-      get :update
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET destroy" do
-    it "returns http success" do
-      get :destroy
-      expect(response).to have_http_status(:success)
-    end
-  end
-
   describe "GET index" do
     it "returns http success" do
       get :index
