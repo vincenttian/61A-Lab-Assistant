37221aa87ba79c4f21257597016ba515beaa3827
Vincent Tian <vincenttian16@gmail.com>
2015-03-13 13:13:12 -0700

    add teaching assistant fo check in form

--- app/models/check_in_form.rb
+++ app/models/check_in_form.rb
@@ -2,12 +2,13 @@
 #
 # Table name: check_in_forms
 #
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  SID        :string(255)
-#  event      :integer
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer          not null, primary key
+#  name               :string(255)
+#  SID                :string(255)
+#  event              :integer
+#  created_at         :datetime
+#  updated_at         :datetime
+#  teaching_assistant :string(255)
 #
 
 class CheckInForm < ActiveRecord::Base
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150313200832) do
+ActiveRecord::Schema.define(version: 20150313201213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20150313200832) do
     t.integer  "event"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "teaching_assistant"
   end
 
   create_table "lab_assistants", force: true do |t|
