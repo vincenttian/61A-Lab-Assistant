467658b4fa66a3308d5037fe591df29657bfaf1d
Vincent Tian <vincenttian16@gmail.com>
2015-03-17 14:44:16 -0700

    fix styling/syntax errors from code climate

--- app/assets/javascripts/components/shared/utilities.js
+++ app/assets/javascripts/components/shared/utilities.js
@@ -2,12 +2,12 @@
 // a human-readable version.
 String.prototype.humanize = function humanize() {
   return this.replace(/_/g, ' ');
-}
+};
 
 // Capitalizes first letter in string
 String.prototype.capitalize = function capitalize() {
   return this.charAt(0).toUpperCase() + this.slice(1);
-}
+};
 
 // Makes a new object with only the pairs where the key is
 // in keys from the original object
@@ -16,22 +16,22 @@ var pick = function pick(obj, keys) {
   obj = new Object(obj);
   for (var i = 0, length = keys.length; i < length; i++) {
     key = keys[i];
-    if (key in obj) result[key] = obj[key];
+    if (key in obj) { result[key] = obj[key]; }
   }
   return result;
-}
+};
 
 // Takes in a context and returns an error handler for forms
 var serverError = function genServerError(context) {
   return function serverError(xhr, status, err) {
     if (xhr.status === 422) {
       console.error(xhr.responseJSON, status, err.toString());
-      this.setState({ errors: xhr.responseJSON })
+      this.setState({ errors: xhr.responseJSON });
     } else {
       toastr.error("There was an error connecting to the server. Please refresh and try again.");
     }
   }.bind(context);
-}
+};
 
 // Takes in a context and and returns a function to be passed into .done() that
 // runs this.setState(data)
@@ -39,11 +39,11 @@ var stateSetter = function (context) {
   return function stateSetter(data) {
     this.setState(data);
   }.bind(context);
-}
+};
 
 // Makes a get request to url and sets context's state to the result
 var getState = function getState(context, url) {
   $.get(url)
    .done(stateSetter(context))
    .fail(serverError(context));
-}
+};
