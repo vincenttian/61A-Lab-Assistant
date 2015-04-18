3c2e48e2cb1e0d2c0d9a6d434e2aedf74917d011
Youri <youri@threadflip.com>
2015-04-17 20:50:13 -0700

    hopefully fixed CI issues

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150417060508) do
+ActiveRecord::Schema.define(version: 20150418034852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20150417060508) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "teaching_assistant"
+    t.integer  "checkins",           default: [], array: true
     t.string   "SID"
     t.integer  "event"
   end
