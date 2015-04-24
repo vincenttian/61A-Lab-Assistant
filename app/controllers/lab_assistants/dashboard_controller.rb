7004c04fc4892442cace5f37a1873e66cf6287aa
Vincent Tian <vincenttian16@gmail.com>
2015-04-23 20:40:04 -0700

    show lab assistants only stuff from their lab

--- app/controllers/lab_assistants/dashboard_controller.rb
+++ app/controllers/lab_assistants/dashboard_controller.rb
@@ -4,8 +4,8 @@ module LabAssistants
     before_filter :authenticate_lab_assistant!
 
     def show
-      @lt = LabTime.all
       @curr_user = current_user
+      @lt = LabTime.where(course_id: @curr_user.course_id)
       @preferred_times = current_user.preferred_lab_times
       @checkins = CheckInForm.where(SID: current_user.SID.to_s)
     end
