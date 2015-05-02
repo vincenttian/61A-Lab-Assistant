bd2aaa47fc582f4f509dbec3df0aa723cf65f20d
Tristan Jones <tristanjones@berkeley.edu>
2015-05-02 00:21:31 -0700

    Slowly adding back tests

--- spec/controllers/check_in_forms_controller_spec.rb
+++ spec/controllers/check_in_forms_controller_spec.rb
@@ -2,10 +2,18 @@ require 'rails_helper'
 
 RSpec.describe CheckInFormsController, :type => :controller do
 
+
   describe "GET new" do
+    before(:each) do
+        @request.env["devise.mapping"] = Devise.mappings[:admin]
+        b = Course.create()
+        a = TeachingAssistant.create(first_name: 'first_name', last_name: 'last_name', email: 'test@test.com', password: 'password', course_id: b.id)
+        sign_in a
+    end
+
     it "returns http success" do
-      # get :new
-      # expect(response).to have_http_status(:success)
+       get :new
+       expect(response).to have_http_status(:success)
     end
   end
 
@@ -15,5 +23,12 @@ RSpec.describe CheckInFormsController, :type => :controller do
       expect(response).to have_http_status(:success)
     end
   end
+
+  #describe "general stuff" do
+  #  it "show returns http success" do
+  #    get :check_in_form_params
+  #    expect(response).to have_http_status(:success)
+  #  end
+  #end
 
 end
