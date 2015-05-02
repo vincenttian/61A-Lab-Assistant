3b9a5d58d7246abf6dbfa3d2c2db7f96e10440b7
Tristan Jones <tristanjones@berkeley.edu>
2015-05-01 23:25:39 -0700

    Another excellent quality test

--- spec/controllers/lab_times_controller_spec.rb
+++ spec/controllers/lab_times_controller_spec.rb
@@ -6,11 +6,12 @@ RSpec.describe LabTimesController, :type => :controller do
       @request.env["devise.mapping"] = Devise.mappings[:admin]
       @ta = TeachingAssistant.create(first_name: 'first_name', last_name: 'last_name', email: 'test@test.com', password: 'password', id: 100)
       @user = Admin.create(first_name: 'first_name', last_name: 'last_name', email: 'test@test.com', password: 'password')
+      @labtime = LabTime.create(open: Time.now(), close: Time.now(), day: Time.now(), course_id: 1)
       sign_in @user
   end
 
   describe "GET LabTimesController" do
-    it "show the pages" do
+    it "create the pages" do
         for course in ['CS61A', 'CS61B', 'CS61C', ''] 
             get :create, :lab_time => {course_id: course, day: 'Monday'}
         end
@@ -18,10 +19,15 @@ RSpec.describe LabTimesController, :type => :controller do
         for dia in ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'] do
             get :create, :lab_time => {course_id: 'CS61A', day: dia}
         end
+    end
 
-
+    it "get the ta" do
+        TeachingAssistant.stub(:find).and_return(@ta)
+        LabTime.stub(:find).and_return(@labtime)
+        get :ta, :teaching_assistant => {id: 1, lab_timd_id: 1}
     end
 
   end
+
 
 end
