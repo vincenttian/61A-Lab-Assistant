10ba11c1a0597c22230949ec251eb90e49ea51e3
bha <brianha@berkeley.edu>
2015-05-02 04:33:23 -0300

    hopefully fix

--- spec/controllers/lab_assistants_controller_spec.rb
+++ spec/controllers/lab_assistants_controller_spec.rb
@@ -1,12 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe LabAssistantsController, :type => :controller do
-    b = Course.create()
-    it "show returns http success" do
+    
+    before(:each) do
 	@request.env["devise.mapping"] = Devise.mappings[:admin]
-	#b = Course.create()
+	b = Course.create()
 	a = LabAssistant.create(first_name: 'first', last_name: 'last', email: 'test@test.com', password: 'password', course_id: b.id)	
 	sign_in a
+    end
+
+    it "show returns http success" do		
 	LabAssistant.stub(:find).and_return(a)
 	get "show", :id => b.id
 	expect(response).to have_http_status(:success)
@@ -20,10 +23,6 @@ RSpec.describe LabAssistantsController, :type => :controller do
 let(:attribs){{first_name: 'first', last_name: 'last', email: 'test@test.com', password: 'password', course_id: b.id}}
 
     it "create returns http failure" do
-	@request.env["devise.mapping"] = Devise.mappings[:admin]
-	#b = Course.create()
-	a = LabAssistant.create(first_name: 'first', last_name: 'last', email: 'test@test.com', password: 'password', course_id: 'CS61A')	
-	sign_in a
 	LabAssistant.stub(:new).and_return(a)
 	for thing in ['CS61A', 'CS61B', 'CS61C', '']
 	    attribs[:course_id] = thing
@@ -35,8 +34,6 @@ let(:attribs){{first_name: 'first', last_name: 'last', email: 'test@test.com', p
 let(:con_par){{first_name: 'first', last_name: 'last', email: 'test@test.com', password: 'password', course_id: '69'}}
 
     it "create_contract returns http failure" do
-	a = LabAssistant.create(first_name: 'first', last_name: 'last', email: 'test@test.com', password: 'password', course_id: b.id)	
-	sign_in a
 	get :create_contract, {:contract => con_par}
 	expect(response).to have_http_status(302)
     end
